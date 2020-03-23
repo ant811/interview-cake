a7b4a22a1a299306b4672b9843390c6397764b8c
Anthony Burroughs <bonfilio.tony@gmail.com>
2020-03-23 18:00:15 -0500

    Added additional edge case tests for MergingMeetingTimes

--- 1_array_&_string_manipulation/merging_meeting_times/mergingMeetingTimes.test.js
+++ 1_array_&_string_manipulation/merging_meeting_times/mergingMeetingTimes.test.js
@@ -1,8 +1,10 @@
 const { mergeRanges } = require('./mergingMeetingTimes');
 
-test('Returns merged rangses of meeting times', () => {
+test('Returns merged ranges of meeting times', () => {
   expect(mergeRanges([{ startTime: 0, endTime: 1 }, { startTime: 3, endTime: 5 }, { startTime: 4, endTime: 8 }, { startTime: 10, endTime: 12 }, { startTime: 9, endTime: 10 },])).toEqual([{ startTime: 0, endTime: 1 }, { startTime: 3, endTime: 8 }, { startTime: 9, endTime: 12 }]);
   expect(mergeRanges([{ startTime: 1, endTime: 3 }, { startTime: 2, endTime: 4 }])).toEqual([{ startTime: 1, endTime: 4 }]);
   expect(mergeRanges([{ startTime: 1, endTime: 5 }, { startTime: 2, endTime: 3 }])).toEqual([{ startTime: 1, endTime: 5 }]);
   expect(mergeRanges([{ startTime: 1, endTime: 8 }])).toEqual([{ startTime: 1, endTime: 8 }]);
+  expect(mergeRanges([{ startTime: 1, endTime: 2 }, { startTime: 2, endTime: 3 }])).toEqual([{ startTime: 1, endTime: 3 }]);
+  expect(mergeRanges([{ startTime: 1, endTime: 10 }, { startTime: 2, endTime: 6 }, { startTime: 3, endTime: 5 }, { startTime: 7, endTime: 9 },])).toEqual([{ startTime: 1, endTime: 10 }]);
 });

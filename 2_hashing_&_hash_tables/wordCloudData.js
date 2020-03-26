c1e323f4576eb95a54a27a8d725b59c644f7f501
Anthony Burroughs <bonfilio.tony@gmail.com>
2020-03-26 13:49:16 -0500

    Refactored wordCloudData solution into class

--- 2_hashing_&_hash_tables/wordCloudData.js
+++ 2_hashing_&_hash_tables/wordCloudData.js
@@ -10,7 +10,6 @@ const isLetter = (char) => {
 const populateWordsToCounts = (inputString) => {
   const result = new Map();
   //const words = [];
-  
   let startIndex = 0;
   let endIndex = 0;
   for (let i = 0; i < inputString.length; i++) {
@@ -33,14 +32,14 @@ const populateWordsToCounts = (inputString) => {
         endIndex++;
       } else {
         if (isLetter(inputString[i - 1])) {
-            let word = inputString.slice(startIndex, endIndex);
-            addWordToMap(result, word);
-            // words.push(inputString.slice(startIndex, endIndex));
-            endIndex++;
-            startIndex = endIndex;
+          let word = inputString.slice(startIndex, endIndex);
+          addWordToMap(result, word);
+          // words.push(inputString.slice(startIndex, endIndex));
+          endIndex++;
+          startIndex = endIndex;
         } else {
-            startIndex++;
-            endIndex++;
+          startIndex++;
+          endIndex++;
         }
       }
     }
@@ -77,7 +76,70 @@ const populateWordsToCounts = (inputString) => {
 // Time complexity: O(n)
 // Space complexity: O(n)
 
-module.exports = { populateWordsToCounts };
-
 // NOTE:
 // Official solution cleverly organized code into a class, tying methods together
+
+
+// UPDATE:  Refactored solution into class
+
+class WordCloudData {
+  constructor(inputString) {
+    this.wordsToCounts = new Map();
+    this.populateWordsToCounts(inputString);
+  }
+
+  populateWordsToCounts(inputString) {
+    let startIndex = 0;
+    let endIndex = 0;
+    for (let i = 0; i < inputString.length; i++) {
+      let char = inputString[i];
+      let letter = this.isLetter(char);
+      if (inputString[i + 1] === undefined) {
+        if (letter) {
+          let word = inputString.slice(startIndex);
+          this.addWordToMap(word);
+        } else {
+          let word = inputString.slice(startIndex, endIndex);
+          this.addWordToMap(word);
+        }    
+      } else {
+        if (letter) {
+          endIndex++;
+        } else if ((char === '-' || char === '\'') && this.isLetter(inputString[i + 1]) && this.isLetter(inputString[i - 1])) {
+          endIndex++;
+        } else {
+          if (this.isLetter(inputString[i - 1])) {
+            let word = inputString.slice(startIndex, endIndex);
+            this.addWordToMap(word);
+            endIndex++;
+            startIndex = endIndex;
+          } else {
+            startIndex++;
+            endIndex++;
+          }
+        }
+      }
+    }
+  }
+
+  isLetter(char) {
+    let isLetter = false;
+    if (char.charCodeAt(0) > 64 && char.charCodeAt(0) < 91 || char.charCodeAt(0) > 96 && char.charCodeAt(0) < 123) {
+      isLetter = true;
+    }  
+  
+    return isLetter;
+  }
+
+  addWordToMap(word) {
+    let first = word[0].toLowerCase();
+    let lowerCaseWord = first + word.slice(1);
+    if (this.wordsToCounts.has(lowerCaseWord)) {
+      this.wordsToCounts.set(lowerCaseWord, this.wordsToCounts.get(lowerCaseWord) + 1);
+    } else {
+      this.wordsToCounts.set(lowerCaseWord, 1);
+    }
+  }
+}
+
+module.exports = { populateWordsToCounts, WordCloudData };
--- 2_hashing_&_hash_tables/wordCloudData.test.js
+++ 2_hashing_&_hash_tables/wordCloudData.test.js
@@ -1,12 +1,11 @@
-const { populateWordsToCounts } = require('./wordCloudData');
+const { populateWordsToCounts, WordCloudData } = require('./wordCloudData');
 
 test('Returns map of word frequency', () => {
   let str1 = 'I like cake';
   let map1 = new Map([['i', 1], ['like', 1], ['cake', 1]]);
   expect(populateWordsToCounts(str1)).toEqual(map1);
   let str2 = 'Chocolate cake for dinner and pound cake for dessert';
-  let map2 = new Map([['and', 1], ['pound', 1], ['for', 2], ['dessert', 1],
-  ['chocolate', 1], ['dinner', 1], ['cake', 2]]);
+  let map2 = new Map([['and', 1], ['pound', 1], ['for', 2], ['dessert', 1], ['chocolate', 1], ['dinner', 1], ['cake', 2]]);
   expect(populateWordsToCounts(str2)).toEqual(map2);
   let str3 = 'Strawberry short cake? Yum!';
   let map3 = new Map([['cake', 1], ['strawberry', 1], ['short', 1], ['yum', 1]]);
@@ -20,4 +19,25 @@ test('Returns map of word frequency', () => {
   let str6 = 'Allie\'s Bakery: Sasha\'s Cakes';
   let map6 = new Map([['bakery', 1], ['cakes', 1], ["allie's", 1], ["sasha's", 1]]);
   expect(populateWordsToCounts(str6)).toEqual(map6);
+});
+
+test('Class Refactor - Returns map of word frequency', () => {
+  let str1 = 'I like cake';
+  let map1 = new Map([['i', 1], ['like', 1], ['cake', 1]]);
+  expect(new WordCloudData(str1).wordsToCounts).toEqual(map1);
+  let str2 = 'Chocolate cake for dinner and pound cake for dessert';
+  let map2 = new Map([['and', 1], ['pound', 1], ['for', 2], ['dessert', 1], ['chocolate', 1], ['dinner', 1], ['cake', 2]]);
+  expect(new WordCloudData(str2).wordsToCounts).toEqual(map2);
+  let str3 = 'Strawberry short cake? Yum!';
+  let map3 = new Map([['cake', 1], ['strawberry', 1], ['short', 1], ['yum', 1]]);
+  expect(new WordCloudData(str3).wordsToCounts).toEqual(map3);
+  let str4 = 'Dessert - mille-feuille cake';
+  let map4 = new Map([['cake', 1], ['dessert', 1], ['mille-feuille', 1]]);
+  expect(new WordCloudData(str4).wordsToCounts).toEqual(map4);
+  let str5 = 'Mmm...mmm...decisions...decisions';
+  let map5 = new Map([['mmm', 2], ['decisions', 2]]);
+  expect(new WordCloudData(str5).wordsToCounts).toEqual(map5);
+  let str6 = 'Allie\'s Bakery: Sasha\'s Cakes';
+  let map6 = new Map([['bakery', 1], ['cakes', 1], ['allie\'s', 1], ['sasha\'s', 1]]);
+  expect(new WordCloudData(str6).wordsToCounts).toEqual(map6);
 });
